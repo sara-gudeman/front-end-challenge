c0288217c1d7ecb995d4d46a390606be65c07922
Sara Gudeman <sgudeman982@gmail.com>
2016-03-24 00:51:33 -0700

    (config) webpack config bundles js + css correctly

---
+++ 1-react-component/client/src/main.js
@@ -0,0 +1,9 @@
+require('./assets/styles.css');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+	<div>Hello</div>,
+	document.getElementById('content')
+);
---
+++ 1-react-component/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'client/src/main.js'),
+	output: {
+		path: path.resolve(__dirname, 'client/dist'),
+		publicPath: '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+		{
+			test: /\.css$/,
+			loader: 'style!css'
+		},
+		{
+			test: /\.jsx?$/,
+			loader: 'babel-loader'
+		}]
+	}
+};
