00c72c2c88c0c67552201889e7b9e45819519d90
Sara Gudeman <sgudeman982@gmail.com>
2016-03-24 16:24:18 -0700

    (components) adds navList, navItems, and content components

--- 1-react-component/client/src/components/NavBar.jsx
+++ 1-react-component/client/src/components/NavBar.jsx
@@ -3,9 +3,33 @@ var ReactDOM = require('react-dom');
 
 var data = require('../assets/data.json');
 
-var nav = {
-	titles: "Hello"
-};
+var Content = React.createClass({
+	render: function () {
+		return (
+			<div>Content</div>
+		);
+	}
+});
+
+var NavItem = React.createClass({
+	render: function () {
+		return (
+			<li>{this.props.section.name}</li>
+		);
+	}
+});
+
+var NavList = React.createClass({
+	render: function () {
+		return (
+			<ul>
+				{this.props.data.map(function (item, index) {
+					return <NavItem key={index} section={item} />
+				})}
+			</ul>
+		);
+	}
+});
 
 var NavBar = React.createClass({
 	render: function () {
