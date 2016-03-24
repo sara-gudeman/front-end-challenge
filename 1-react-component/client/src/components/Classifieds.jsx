44c21cbbef3a0300f50017643bbd4708b51cea36
Sara Gudeman <sgudeman982@gmail.com>
2016-03-24 16:51:42 -0700

    (components) moves content into own component

--- 1-react-component/client/src/components/Classifieds.jsx
+++ 1-react-component/client/src/components/Classifieds.jsx
@@ -1,12 +1,13 @@
 var React = require('react');
 var NavBar = require('./NavBar/NavBar.jsx');
+var Content = require('./Content.jsx');
 
 var Classifieds = React.createClass({
 	render: function () {
 		return (
 			<div>
 				<NavBar data={this.props.data} />
-				<div>Content</div>
+				<Content />
 			</div>
 		);
 	}
---
+++ 1-react-component/client/src/components/Content.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Content = React.createClass({
+	render: function () {
+		return (
+			<div>Hello</div>
+		);
+	}
+});
+
+module.exports = Content;
