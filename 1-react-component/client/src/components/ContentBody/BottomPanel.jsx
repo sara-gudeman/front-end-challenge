db1c021762d278a1a6a3b8b1cf01bd1087cdbdab
Sara Gudeman <sgudeman982@gmail.com>
2016-03-25 10:10:16 -0700

    (component) WIP - making bottom advert panel

---
+++ 1-react-component/client/src/components/ContentBody/BottomPanel.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var BottomPanel = React.createClass({
+	render: function () {
+		return (
+			<div className="content_panel">
+				Some Advertisement image
+				Some Advertisement section links
+				Some Advertisement mini pic
+			</div>
+		);
+	}
+});
+
+module.exports = BottomPanel;
--- 1-react-component/client/src/components/ContentBody/ContentBody.jsx
+++ 1-react-component/client/src/components/ContentBody/ContentBody.jsx
@@ -1,13 +1,14 @@
 var React = require('react');
 var ContentPanel = require('./ContentPanel.jsx');
 var TopPanel = require('./TopPanel.jsx');
+var BottomPanel = require('./BottomPanel.jsx');
 
 var ContentBody = React.createClass({
 	render: function () {
 		return (
 			<div>
 				<TopPanel />
-				<ContentPanel />
+				<BottomPanel />
 			</div>
 		);
 	}
